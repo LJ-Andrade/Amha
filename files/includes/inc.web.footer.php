30cb3274a86c1181806246ba8fd73e3cd56ab88b
Lea <Lea>
2016-06-07 03:29:42 -0300

    Footer

--- files/includes/inc.web.footer.php
+++ files/includes/inc.web.footer.php
@@ -14,12 +14,11 @@
       </div>
       <div class="col-md-4">
         <div class="footerData">
-              <p>Turnos: <b>4827-2907</b> <br>
-              Administraci&oacute;n: <b>4826-5852</b> <br>
-              Escuela, Fax. <b>4827-2907</b><br>
-              <a href="mailto:info@amha.org.ar">info@amha.org.ar</a></p>
-              <p><b>Juncal 2884 </b>| Buenos Aires | Argentina</p>
-
+          <p>Turnos: <b>4827-2907</b> <br>
+          Administraci&oacute;n: <b>4826-5852</b> <br>
+          Escuela, Fax. <b>4827-2907</b><br>
+          <a href="mailto:info@amha.org.ar">info@amha.org.ar</a></p>
+          <p><b>Juncal 2884 </b>| Buenos Aires | Argentina</p>
         </div>
       </div>
     </div>
