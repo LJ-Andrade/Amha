07814a96092e52fb1c9401ab049fb0bacc888721
Alejandro Romero <romero.m.alejandro@gmail.com>
2016-08-23 18:02:13 -0300

    _

--- files/modules/web/consultoriosprivados.php
+++ files/modules/web/consultoriosprivados.php
@@ -1,3 +1,36 @@
+<?php
+  include("../../classes/class.database.php");
+  //echo "1";
+  $DB = new DataBase();
+  //var_dump($DB);die();
+  $Doctors = $DB->fetchAssoc('doctor','*');
+  //var_dump($DB);die();
+  foreach($Doctors as $Doctor)
+  {
+    $Title    = $Doctor['sex']=='M'? 'Dr.':'Dra.';
+    $Name     = $Title." ".ucfirst(utf8_encode($Doctor['last_name'].", ".$Doctor['first_name']));
+    $MN       = $Doctor['national_medical_enrollment'] ? '<br>'.'Matricula Nacional: '.$Doctor['national_medical_enrollment'].'<br>':'';
+    $MP       = $Doctor['provincial_medical_enrollment'] ? '<br>'.'Matricula Provincial: '.$Doctor['provincial_medical_enrollment'].'<br>':'';
+    $Email    = $Doctor['email']? '<br>'.strtolower($Doctor['email']).'<br>':'';
+    $Website  = $Doctor['website']? '<br>'.strtolower($Doctor['website']).'<br>':'';
+    $HTML    .= '
+    <div class="row wow zoomIn fadeIn">
+      <div class="col-sm-12 itemContainer">
+        <div class="card-header"><h5 class="card-title">'.$Name.'</h5></div>
+        <div class="card card-block">
+          <p class="card-text">
+            '.utf8_encode($Doctor['description']).'
+            <br>
+            '.$Email.'
+            '.$Website.'
+            '.$MN.'
+            '.$MP.'
+          </p>
+        </div>
+      </div>
+    </div>';
+  }
+ ?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -35,8 +68,7 @@
                 <select class="form-control" id="sel1">
                   <option>Provincia</option>
                   <option>Buenos Aires</option>
-                  <option>Capital</option>
-                  <option>Rivendel</option>
+                  <option>Capital Federal</option>
                   <option>C&oacute;rdoba</option>
                 </select>
                 <!-- // In case you dont want a select, here is a simple input good man // -->
@@ -58,152 +90,7 @@
               <!-- <div class="col-sm-3 col-xs-12"><button type="button" class="btn searchBtn"><i class="fa fa-search"></i> Buscar</button></div> -->
             </div>
           </div>
-          <div class="row wow zoomIn fadeIn">
-            <div class="col-sm-12 itemContainer">
-              <div class="card-header"><h5 class="card-title">Dra. ATENOR PATRICIA</h5></div>
-              <div class="card card-block">
-                <p class="card-text">
-                  MN 73.564<br>
-                  Ayudante de C&aacute;tedra en la AMHA<br>
-                  Oftalmolog&iacute;a<br>
-                  Av. Callao 671, 5to piso departamento A, Once, Buenos Aires<br>
-                  Pedir turnos al: 4372-8703<br><br>
-                </p>
-              </div>
-            </div>
-            <div class="col-sm-12 itemContainer">
-              <div class="card-header"><h5 class="card-title">Dra. CALVI ANDREA MARA</h5></div>
-              <div class="card card-block">
-                <p class="card-text">
-                  MN 74482  <br>
-                  Ayudante de C&aacute;tedra en AMHA<br>
-                  Medicina Laboral<br>
-                  Av. Francisco Beir&oacute; 4355, Villa Devoto, CABA<br>
-                  Pedir turnos al 4501-8597<br>
-                  Martes y Jueves de 17 a 19hs.<br>
-                </p>
-              </div>
-            </div>
-            <div class="col-sm-12 itemContainer">
-              <div class="card-header"><h5 class="card-title">Dr. CAMPANELLI ADOLFO </h5></div>
-              <div class="card card-block">
-                <p class="card-text">
-                  MN 44415<br>
-                  Profesor adjunto en la AMHA<br>
-                  Vidt 1923 3º A, Capital Federal<br>
-                  Pedir turnos al 4826-5652<br>
-                  Horarios: Un Martes de 14 a 19 hs. Otro de 11 a 19 hs.<br>
-                  Rafaela 4650, Villa Luro, Capital Federal<br>
-                  Pedir Turnos al  15-4449-8668<br>
-                  Horarios: Mi&eacute;rcoles de 14 a 20 hs.<br>
-                </p>
-              </div>
-            </div>
-            <div class="col-sm-12 itemContainer">
-              <div class="card-header"><h5 class="card-title">Dra. CAPELLA INES M. C.</h5></div>
-              <div class="card card-block">
-                <p class="card-text">
-                  MN 66446 <br>
-                  M&eacute;dica Cl&iacute;nica Oftalmolog&iacute;a<br>
-                  Cabildo 2327 1°D<br>
-                  Belgrano, CABA<br>
-                  Pedir turno al 4785-4993<br>
-                  Horarios: Jue de 17 a 20 hs y Vie de 15 a 20 hs<br>
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="row wow zoomIn fadeIn">
-            <div class="col-sm-12 itemContainer">
-              <div class="card-header"><h5 class="card-title">Dra. CHAPIRO CLARA SILVIA</h5></div>
-              <div class="card card-block">
-                <p class="card-text">
-                  MN: 45721<br>
-                  Ayudante de C&aacute;tedra en la AMHA<br>
-                  Psicoterapeuta Ni&ntilde;os y Adolescentes<br>
-                  Mendoza 5180 2º A, Villa Urquiza<br>
-                  Pedir turnos al 4522-7615 o 15-3085-1408<br><br>
-                </p>
-              </div>
-            </div>
-            <div class="col-sm-12 itemContainer">
-              <div class="card-header"><h5 class="card-title">Dra. CHAPOCHNIKOFF MARIANA</h5></div>
-              <div class="card card-block">
-                <p class="card-text">
-                  MN 92172<br>
-                  Vuelta de Obligado 1441 6º C, Belgrano<br>
-                  Pedir turnos al 4553-0591<br>
-                  Urgencias: 11-6478-0285<br>
-                  mchapo@intramed.net<br>
-                  www.guiahomeopatica.com.ar<br>
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="row wow zoomIn fadeIn">
-            <div class="col-sm-12 itemContainer">
-              <div class="card-header"><h5 class="card-title">Dra. CIOLA MARIA DELIA</h5></div>
-              <div class="card card-block">
-                <p class="card-text">
-                  MN 84038<br>
-                  Cl&iacute;nica y Reumatolog&iacute;a<br>
-                  Charcas 2912 8ºA<br>
-                  Recoleta, Buenos Aires<br>
-                  Pedir turnos al 4827-4657<br>
-                  Horarios: Lunes y Jueves de 14 a 20 hs o Mie y Vie de 10 a 16 hs.<br>
-                  Celular: 15-56390784<br>
-                  saludinteractiva@yahoo.com.ar<br>
-                  www.draciolamaria.com.ar<br><br>
-                </p>
-              </div>
-            </div>
-            <div class="col-sm-12 itemContainer">
-              <div class="card-header"><h5 class="card-title">Dr. DIAZ CAMPOS ROBERTO LUIS</h5></div>
-              <div class="card card-block">
-                <p class="card-text">
-                  M.N.: 74.562<br>
-                  M.P.: 37.490<br>
-                  Profesor Adjunto del A.M.H.A.<br>
-                  drdiazcampos@fibertel.com.ar<br>
-
-                  Avellaneda 607 - Caballito.<br>
-                  Horarios: lunes de 15 a 20 hs.<br>
-                  Tel&eacute;fono: 5901-5420<br>
-
-                  Charcas 2744, 2º Piso, Depto 6, Barrio Norte<br>
-                  Horarios: viernes de 9 a 17 hs.<br>
-                  Tel&eacute;fono: 4962-6812<br>
-                </p>
-              </div>
-            </div>
-            <div class="col-sm-12 itemContainer">
-              <div class="card-header"><h5 class="card-title">Dr. DRAIMAN MARIO</h5></div>
-              <div class="card card-block">
-                <p class="card-text">
-                  MN: 30.308 <br>
-                  Gallo 1576. Piso 7º Depto B - Palermo <br>
-                  Teléfono: 4826-5922 <br>
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="row wow zoomIn fadeIn">
-            <div class="col-sm-12 itemContainer">
-              <div class="card-header"><h5 class="card-title">Dr. DUCASSE CARLOS DANIEL</h5></div>
-              <div class="card card-block">
-                <p class="card-text">
-                  MN 65218 <br>
-                  Clínica Médica-Obesidad <br>
-                  Juramento 4468 P.B <br>
-                  V. Urquiza <br>
-                  4521-9994 y 4523-1094 <br>
-                  Martes y Jueves de 15:30 a 21hs <br>
-                  doctorducasse@yahoo.com.ar <br>
-                  www.institutodemedicinaalternativa.com <br>
-                </p>
-              </div>
-            </div>
-          </div>
+          <?php echo $HTML; ?>
         </div><!-- /contentContainer -->
         <?php include('sidebar.php'); ?><!-- Right Sidebar -->
       </div><!-- /MainContainer --><!-- Content inside this div -->
