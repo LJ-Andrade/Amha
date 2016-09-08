e227894c0c54fff8e9f4d1f70f85713c6171f427
Alejandro Romero <romero.m.alejandro@gmail.com>
2016-09-08 09:05:04 +0000

    Medic list updated

--- files/modules/web/consultoriosprivados.php
+++ files/modules/web/consultoriosprivados.php
@@ -8,7 +8,7 @@
     $Where = "AND (z.title LIKE '%".$V."%' OR p.title LIKE '%".$V."%' OR (d.doctor_id = r.doctor_id AND r.specialty_id = s.specialty_id AND s.title LIKE '%".$V."%') OR d.first_name LIKE '%".$V."%' OR o.address LIKE '%".$V."%' OR o.phone LIKE '%".$V."%' OR d.last_name LIKE '%".$V."%' OR d.description LIKE '%".$V."%' OR d.national_medical_enrollment LIKE '%".$V."%'  OR d.provincial_medical_enrollment LIKE '%".$V."%'  OR d.email LIKE '%".$V."%'  OR d.website LIKE '%".$V."%')";
   }
   $Doctors = $DB->execQuery("free","SELECT d.* FROM doctor as d, doctor_office as o, country_province as p, country_zone as z, doctor_specialty as s, relation_doctor_specialty AS r WHERE d.doctor_id = o.doctor_id AND o.province_id = p.province_id AND o.zone_id = z.zone_id ".$Where." GROUP BY d.doctor_id");
-  echo count($Doctors);
+  var_dump($Doctors);
   foreach($Doctors as $Doctor)
   {
     switch ($Doctor['type'])
