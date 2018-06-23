dbbcb83ca0da7b27c67ceba70381758b5a8481e3
Alejandro Romero <romero.m.alejandro@gmail.com>
2018-06-22 22:24:27 -0300

    Se agrega .gitignore

--- files/classes/class.configuration.php
+++ files/classes/class.configuration.php
@@ -2,7 +2,7 @@
 
 class Configuration extends DataBase
 {
-	
+
 	var $Data;
 	var $Table			= "configuration";
 
@@ -10,15 +10,14 @@ class Configuration extends DataBase
 	{
 		$this->Connect();
 		$Data = $this->fetchAssoc($this->Table,"*");
-		$this->Data = $Data[0];
-		
+		$this->Data = $Data[0];	
 	}
-	
+
 	public function GetData()
 	{
 		return $this->Data;
 	}
-	
+
 	public function Update()
 	{
 		// Basic Data
@@ -28,7 +27,7 @@ class Configuration extends DataBase
 		$Data4 = $_POST['data_4'];
 		$Data5 = $_POST['data_5'];
 		$Data6 = $_POST['data_6'];
-		
+
 		$Update	= $this->execQuery('update',$this->Table,"data_1='".$Data1."',data_2='".$Data2."',data_3='".$Data3."',data_4='".$Data4."',data_5='".$Data5."',data_6='".$Data6."'");
 		//echo $this->lastQuery();
 	}
